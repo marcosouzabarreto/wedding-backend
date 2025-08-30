d7911c61ca63737666bc91087466b5702a7f00a6
Marco Barreto <marcosouzabarreto@gmail.com>
2025-08-30 18:56:55 -0300

    fix: Fix autoincrement

--- models/gift.go
+++ models/gift.go
@@ -1,7 +1,7 @@
 package models
 
 type Gift struct {
-	ID          string  `json:"id" gorm:"autoincrement"`
+	ID          uint    `json:"id" gorm:"autoincrement"`
 	Name        string  `json:"name"`
 	Description string  `json:"description"`
 	Price       float64 `json:"price"`
